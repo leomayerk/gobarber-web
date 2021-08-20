e139f89bbf0adf281d90626fd3a6e972f53403ae
Leonardo Mayer Kleesattel <leomayerk@hotmail.com>
2021-08-20 16:14:26 -0300

    Next schedule

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 import {
-  Container, Header, HeaderContent, Profile,
+  Container, Header, HeaderContent, Profile, Content, Schedule, Calendar, NextAppointment,
 } from './styles';
 
 const Dashboard: React.FC = () => {
@@ -27,6 +27,30 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="https://avatars.githubusercontent.com/u/55405433?s=60&v=4" alt="Leonardo Mayer" />
+              <strong>Leonardo Mayer</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
